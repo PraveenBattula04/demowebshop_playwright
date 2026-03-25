da5472852335f4b1e36e43e7279782d12a87d6a2
praveen.battula04 <praveen.battula04@gmail.com>
2026-03-25 14:11:49 +0530

    login testcase

---
+++ mytests/login.spec.ts
@@ -0,0 +1,25 @@
+import {test, expect, Browser, chromium, Page, Locator } from '@playwright/test';
+import { Header } from '../modules/header.ts';
+import { ReturningCustomer } from '../modules/returningCustomer.ts';
+const username:string = 'iambatman004@gmail.com';
+const password:string = 'Abcd@1234';
+const books:[string, string] = ['Fiction', 'Health Book'];
+
+test('login', async ({page}) => {
+    // const broswer: Browser = await chromium.launch({headless: false})
+    // const page: Page = await broswer.newPage();
+    // await page.goto("url");
+    await page.goto('https://demowebshop.tricentis.com/');
+    const header = new Header(page);
+    const returningCustomer = new ReturningCustomer(page);
+    await (await header.headerfunc('loginLink')).click();
+    const emailField: Locator = await returningCustomer.returningCustomerfunc('email');
+    await expect(emailField).toBeVisible();
+    await emailField.fill(username);
+    await (await returningCustomer.returningCustomerfunc('password')).fill(password);
+    // await (await returningCustomer.returningCustomerfunc('remembermeCheckbox')).click();
+    // await (await returningCustomer.returningCustomerfunc('forgotPasswordLink')).click();
+    await (await returningCustomer.returningCustomerfunc('loginButton')).click();
+    page.close();
+    // await browser.close();
+})
